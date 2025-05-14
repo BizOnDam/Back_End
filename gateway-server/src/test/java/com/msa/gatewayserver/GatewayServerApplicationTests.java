60db5d2733464333337813c6f322864bba0cd474
hyunseoko <kohyun0223@skuniv.ac.kr>
2025-05-14 11:06:15 +0900

    :sparkles:Feat: gateway 서버 수정

--- gateway-server/src/test/java/com/msa/gatewayserver/GatewayServerApplicationTests.java
+++ gateway-server/src/test/java/com/msa/gatewayserver/GatewayServerApplicationTests.java
@@ -1,13 +1,13 @@
-package com.msa.gatewayservice;
+package com.msa.gatewayserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GatewayServiceApplicationTests {
+class GatewayServerApplicationTests {
 
-  @Test
-  void contextLoads() {
-  }
+	@Test
+	void contextLoads() {
+	}
 
 }
