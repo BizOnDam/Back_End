1966cf0cb64c47845c79b84d261687ecaa9ba723
hyunseoko <kohyun0223@skuniv.ac.kr>
2025-06-13 10:31:59 +0900

    :sparkles:Feat: 아이디 중복 검사 추가

--- user-service/src/main/java/com/bizondam/userservice/controller/UserController.java
+++ user-service/src/main/java/com/bizondam/userservice/controller/UserController.java
@@ -11,10 +11,10 @@ import com.bizondam.userservice.service.EmailAuthService;
 import com.bizondam.userservice.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,12 +39,25 @@ public class UserController {
     return ResponseEntity.ok(BaseResponse.success("회원가입에 성공했습니다.", resp));
   }
 
+  @Operation(summary = "아이디 중복 검사", description = "같은 아이디로 가입 불가능")
+  @GetMapping("/check-login-id")
+  public ResponseEntity<BaseResponse<Boolean>> checkLoginId(@RequestParam String loginId) {
+    boolean isDuplicate = userService.isLoginIdDuplicate(loginId);
+    if (isDuplicate) {
+      return ResponseEntity.ok(BaseResponse.success("이미 사용 중인 아이디입니다.", true));
+    } else {
+      return ResponseEntity.ok(BaseResponse.success("사용 가능한 아이디입니다.", false));
+    }
+  }
+
+  @Operation(summary = "이메일 인증(유효기간 10분)", description = "이메일로 랜덤 6자리 인증번호 발송")
   @PostMapping("/email-auth")
   public ResponseEntity<BaseResponse<Void>> sendEmailAuthCode(@RequestBody EmailSendRequest request) {
     emailAuthService.createAndSendAuthCode(request.getEmail());
     return ResponseEntity.ok(BaseResponse.success("인증코드가 발송되었습니다.", null));
   }
 
+  @Operation(summary = "인증번호 검증", description = "DB에 존재하는 인증번호와 일치하는지 검사")
   @PostMapping("/email-auth/verify")
   public ResponseEntity<BaseResponse<Boolean>> verifyEmailAuthCode(@RequestBody EmailVerifyRequest request) {
     boolean verified = emailAuthService.isVerified(request.getEmail(), request.getCode());
