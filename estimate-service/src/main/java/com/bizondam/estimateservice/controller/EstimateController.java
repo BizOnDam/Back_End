b9fb720b1bc744732d71be4e68105882612dadf4
hyunseoko <kohyun0223@skuniv.ac.kr>
2025-06-18 00:00:26 +0900

    :heavy_plus_sign:Dependency: 빠진 의존성 추가

--- estimate-service/src/main/java/com/bizondam/estimateservice/controller/EstimateController.java
+++ estimate-service/src/main/java/com/bizondam/estimateservice/controller/EstimateController.java
@@ -12,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -65,5 +66,4 @@ public class EstimateController {
     estimateService.rejectBySupplier(requestId, supplierUserId);
     return ResponseEntity.ok().build();
   }
-
 }
